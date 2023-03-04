435290aeada62d6c779d0170c618f68565a5bd0d
Pat Fehling <pdfehling@uwalumni.com>
2023-03-04 06:40:27 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Patrick Fehling on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
